ced511d34eeef0465f46cbcf1003b753e4f61733
Swapnali-zvolv <Swapnali@zvolv.com>
2024-03-07 21:39:59 +0530

    fix: added sample test for form and submission

--- sample.ts
+++ sample.ts
@@ -14,10 +14,37 @@ async function testClient() {
 
         console.log('login', login);
 
-        const analytics = await client.analytics.search('65c470f6dab3102c930725ca',  {query: {match_all: {}}, from: 0, size: 20, track_total_hits: true});
+        const analytics = await client.analytics.search('65c470f6dab3102c930725ca', { query: { match_all: {} }, from: 0, size: 20, track_total_hits: true });
 
         console.log('analytics', analytics);
-        // Add your code here to process the fetched data
+        // Fetch form details with the specified form ID
+        const formId = '65c470f6dab3102c930725ca'; // Replace with the appropriate form ID
+        const form = await client.form.fetch(formId);
+        console.log('form', form);
+
+        // Submission creation data
+        const submissionData = {
+            formId: '65c470f6dab3102c930725ca', // Replace with the appropriate formId
+            elements: [
+                {
+                    label: 'Name',
+                    value: 'Test'
+                }
+            ]
+        };
+
+        // Define query parameters for the submission creation request.
+        // If not provided, default values of false will be used for skipAutomation and skipValidations.
+        const queryParams = {
+            skipAutomation: true,
+            skipValidations: false,
+        };
+
+        // Create a submission using the submissionData
+        const createdSubmission = await client.submission.create(submissionData, queryParams);
+
+        console.log('createdSubmission', createdSubmission);
+
     } catch (error) {
         console.error('Error fetching data:', error);
     }
